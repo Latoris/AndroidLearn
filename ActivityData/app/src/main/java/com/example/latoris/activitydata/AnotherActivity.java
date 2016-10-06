48d9b0a94c291432df79e73dfeb0c2c168fbe7b8
Latoris <renjietang009649@gmail.com>
2016-10-06 15:16:21 +0800

    2

---
+++ ActivityData/app/src/main/java/com/example/latoris/activitydata/AnotherActivity.java
@@ -0,0 +1,29 @@
+package com.example.latoris.activitydata;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+        Button button=(Button)findViewById(R.id.back);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=getIntent();
+                String name=intent.getStringExtra("name");
+                Integer age=intent.getIntExtra("age", 20);
+                intent.putExtra("result","姓名:"+name+" 年龄"+age);
+                setResult(0,intent);
+                finish();
+            }
+        });
+
+    }
+}
